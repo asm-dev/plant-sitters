5183978f8f9f4193305bf49fa8a0d695397dcf40
asm-dev <anasalamancamontero@gmail.com>
2025-02-03 19:32:55 +0000

    add additional generic views to users

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import ActiveUserListView, UserListCreateView, UserRetrieveDestroyView, UserRetrieveUpdateView, UserViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/list-create/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/update/', UserRetrieveUpdateView.as_view(), name='user-update'),
+    path('users/<int:pk>/delete/', UserRetrieveDestroyView.as_view(), name='user-delete'),
+    path('users/active/', ActiveUserListView.as_view(), name='active-users'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,33 @@
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    RetrieveDestroyAPIView,
+)
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from .models import User
 from .serializers import UserSerializer
 
-class UserViewSet(ModelViewSet):
+class UserBaseView:
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserListCreateView(UserBaseView, ListCreateAPIView):
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['is_plantsitter', 'is_requester']
+    search_fields = ['username', 'email']
+
+class UserRetrieveUpdateView(UserBaseView, RetrieveUpdateAPIView):
+    pass
+
+class UserRetrieveDestroyView(UserBaseView, RetrieveDestroyAPIView):
+    pass
+
+class ActiveUserListView(UserBaseView, ListCreateAPIView):
+    queryset = User.objects.filter(is_active=True)
+
+class UserViewSet(UserBaseView, ModelViewSet):
+    pass
